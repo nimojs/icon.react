f6b55e161658de8bfd48bccb68f1680a0623cbe7
Grifree <1149290648@qq.com>
2018-04-04 15:49:27 +0800

    onface/module auto-generated commit

---
+++ 0.2.1/doc/Basic.js
@@ -0,0 +1,33 @@
+var React = require('react')
+var Icon = require('icon.react')
+class Basic extends React.Component {
+    render () {
+        return (
+            <div className="basicDemo" >
+                <Icon type="up" />
+                {' '}
+                <Icon type="up-o" />
+                {' '}
+                <Icon type="up-of" />
+                {' '}
+                <Icon type="up-s" />
+                {' '}
+                <Icon type="up-sf" />
+                {' '}
+                <Icon type="onface" />
+                {' '}
+                <Icon type="info" />
+                {' '}
+                <Icon type="info-o" />
+                {' '}
+                <Icon type="info-of" />
+                {' '}
+                <Icon type="loading" />
+                {' '}
+                <Icon type="github" themes="demo" />
+            </div>
+        )
+    }
+}
+/*ONFACE-DEL*/Basic = require("react-hot-loader").hot(module)(Basic)
+module.exports = Basic
